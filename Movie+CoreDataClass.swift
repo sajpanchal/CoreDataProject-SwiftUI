f0a8a74cbf920fdba48e2f9dd3265894e51f1bbc
Saj Panchal <sajpanchal@gmail.com>
2021-07-27 14:06:01 -0400

    project created with inital test codes

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by saj panchal on 2021-07-27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
