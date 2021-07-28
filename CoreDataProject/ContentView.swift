bddd045a6016359e77aa6a943a2ad0df877b0330
Saj Panchal <sajpanchal@gmail.com>
2021-07-28 12:06:16 -0400

    learning filter coreData with NSPredicate.

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -10,24 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens","Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
     var body: some View {
         VStack {
-            List(wizards, id:\.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown")
             }
             Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
-            }
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                    
-                }
-                catch {
-                    print(error.localizedDescription)
-                }
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star wars"
+                
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star wars"
+                
+                try? self.moc.save()
             }
         }
     }
